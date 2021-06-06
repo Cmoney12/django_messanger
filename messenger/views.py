261e8260ba8ff87b34d16d337e2c9c7dd2fa5040
corey <coreylovette@gmail.com>
2021-06-05 19:25:01 -0500

    added registration page

--- messenger/views.py
+++ messenger/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from . import forms
@@ -8,6 +9,13 @@ from . import forms
 def login(request):
     if request.method == 'POST':
         form = forms.LoginForm()
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+        else:
+            return render(request, 'messenger/login.html')
     else:
         return render(request, 'messenger/login.html')
 
